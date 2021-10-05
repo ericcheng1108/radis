871bbe016e576abd760520c17add689b51d9169d
Eric Cheng <eric.cheng@shoplineapp.com>
2021-10-05 15:54:48 +0800

    Init

---
+++ lib/radis/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Radis
+  VERSION = '0.0.1'
+end
---
+++ spec/radis_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Radis do
+  it "has a version number" do
+    expect(Radis::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
